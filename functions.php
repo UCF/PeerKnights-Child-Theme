a1515dd2ef56b24cb40e793da3efb1ebd847699f
Joseph Giron <jgiron@ucf.edu>
2022-04-12 16:15:32 -0400

    initial commit

---
+++ functions.php
@@ -0,0 +1,18 @@
+<?php
+define( 'DC_THEME_DIR', trailingslashit( get_stylesheet_directory() ) );
+
+include_once DC_THEME_DIR . 'includes/header-functions.php';
+
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+function my_theme_enqueue_styles() {
+ 
+    $parent_style = 'Colleges-Theme-style';
+ 
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/static/css/style.min.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/css/custom.css?'.rand(111,9999),
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
+}
+?>
